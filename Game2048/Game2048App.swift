b3e51538576294c2e364aec484522ab325869cc5
nikita <nikita.dmd@gmail.com>
2022-03-28 23:33:05 +0300

    Initial Commit

---
+++ Game2048/Game2048App.swift
@@ -0,0 +1,17 @@
+//
+//  Game2048App.swift
+//  Game2048
+//
+//  Created by xubuntus on 28.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Game2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
